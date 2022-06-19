9bdc186af79d5f1f5c045a380d72428deef8ccbd
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2022-06-19 18:06:00 +0000

    Version 1.1.5

--- no_finalize.py
+++ no_finalize.py
@@ -12,7 +12,7 @@ to register per-thread cleanup callbacks without coordination.
 """
 
 __all__ = ('ref',)
-__version__ = '1.1.4'
+__version__ = '1.1.5'
 
 
 from threading import current_thread as _current_thread
